647faa260e65764a2a6a9cd2a5feae008339fac6
Andrew <aschulak@gmail.com>
2013-10-27 15:32:43 -0400

    perk api

---
+++ growlengine/growl/api/perk.py
@@ -0,0 +1,29 @@
+import logging
+from growl.models import Perk
+from growl.caches.perk import get_perk_from_cache
+from growl.caches.perk import store_perk_in_cache
+
+logger = logging.getLogger(__name__)
+
+### Create perk
+def create_perk(game, name, description, choosable=True):
+    perk = Perk()
+    perk.game = game
+    perk.name = name
+    perk.description = description
+    perk.choosable = choosable
+    perk.save() # post_save stores in cache
+    return perk
+
+### Get perk
+def get_perk(perk_id):
+    perk = get_perk_from_cache(perk_id)
+    if not perk:
+        perk = Perk.objects.get(pk=perk_id)
+        store_perk_in_cache(perk)
+    return perk
+
+### Update perk
+def update_perk(perk):
+    perk.save() # post_save stores in cache
+    return perk
