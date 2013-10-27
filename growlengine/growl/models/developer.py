aab54aca04493ef34c39c6d6d810aa3d6bb8401c
Andrew <aschulak@gmail.com>
2013-10-26 21:43:56 -0400

    post save

--- growlengine/growl/models/developer.py
+++ growlengine/growl/models/developer.py
@@ -1,5 +1,14 @@
+import logging
+
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from growl.models.basemodel import BaseModel
+
+logger = logging.getLogger(__name__)
+
+class DeveloperManager(models.Manager):
+    pass
 
 class Developer(BaseModel):
     # TODO make unique
@@ -13,6 +22,7 @@ class Developer(BaseModel):
     activated = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True, auto_now_add=True)
+    objects = DeveloperManager()
 
     class Meta:
         db_table = 'growl_developer'
@@ -30,3 +40,8 @@ class Developer(BaseModel):
         #json['modified'] = str(self.modified)
 
         return json
+
+def post_save_cache(sender, **kwargs):
+    logger.debug('post save!')
+
+post_save.connect(post_save_cache, sender=Developer)
--- growlengine/growlengine/settings.py
+++ growlengine/growlengine/settings.py
@@ -131,31 +131,33 @@ INSTALLED_APPS = (
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s <%(module)s> %(message)s'
+        },
     },
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
     },
     'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
+        '': {
+            'handlers': ['console'],
             'propagate': True,
+            'level': 'DEBUG',
         },
     }
 }
